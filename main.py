b84d590951de336dd321f69bcbab6c8e83e419c4
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-27 14:48:31 +0000

    Notes_Lizveth

---
+++ main.py
@@ -0,0 +1,12 @@
+#function- lock of code that can be called open
+#def- defines a function 
+def my_Function():
+  print("hello from a function")
+#end my_Function
+#Able to call function mutiple times
+#arguments= 
+def my_functions(x,y,z):
+  print("Hello...")
+  c= 0
+  c= (x+y)*z
+  return c
